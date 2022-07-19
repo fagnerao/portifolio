9c9675c001f9b861a478397e64a043aab70971c5
Fagner Araújo <fagnersi@gmail.com>
2022-07-19 10:54:12 -0300

    whatsapp og:type

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,12 @@ export default class MyDocument extends Document {
    
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet"/>
     <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+    <meta property="og:title" content="" />
+    <meta property="og:description" content="Ajudamos sua empresa a encontrar com seus clientes.
+                                             Criação de websites, Lojas on-line e Sistemas web." />
+    <meta property="og:type" content="article" />
+    <meta property="og:url" content="https://vector-zero.vercel.app/" />
+    <meta property="og:image" content="/images/img_w.jpg" />
     </Head>
     <body>
       <Main />
